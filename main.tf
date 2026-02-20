4c7d496907694083f19774e1768cb8f88e684d9b
ChachareVishakha <chachare.vishakha@gmail.com>
2026-02-20 16:44:16 +0530

    uplaod updated file main.tf and newly created file provision_steps.txt

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-# create vpc
+# ---------------- VPC ----------------
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -11,18 +11,19 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
-# create public subnet
+# ---------------- Public Subnet ----------------
 resource "aws_subnet" "public_sub1" {
-    map_public_ip_on_launch = true
-    cidr_block = "10.0.1.0/24"
-    vpc_id = aws_vpc.myvpc.id
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-    tags = {
-      Name = "public_subnet1"
-    }
+  tags = {
+    Name = "public_subnet1"
+  }
 }
 
-# internet gateway
+# ---------------- Internet Gateway ----------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.myvpc.id
 
@@ -31,25 +32,27 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# route table
+# ---------------- Route Table ----------------
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.myvpc.id
-  tags = {
-    Name = "route1"
-  }
-  route  {
+
+  route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+
+  tags = {
+    Name = "route1"
+  }
 }
 
-# route table association
+# ---------------- Route Table Association ----------------
 resource "aws_route_table_association" "assoc" {
-  subnet_id = aws_subnet.public_sub1.id
+  subnet_id      = aws_subnet.public_sub1.id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# Security Group
+# ---------------- Security Group ----------------
 resource "aws_security_group" "web_sg" {
   name   = "web_sg"
   vpc_id = aws_vpc.myvpc.id
@@ -82,25 +85,17 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# Key Pair
-resource "aws_key_pair" "mykey" {
-  key_name   = "terraform_key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-# Ec2 Instance
+# ---------------- EC2 Instance ----------------
 resource "aws_instance" "web_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 us-east-1
-  instance_type = "t2.micro"
-
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_sub1.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name               = aws_key_pair.mykey.key_name
 
   user_data = <<-EOF
 #!/bin/bash
 yum update -y
-yum install nginx -y
+yum install -y nginx
 systemctl start nginx
 systemctl enable nginx
 
@@ -117,9 +112,13 @@ EOF
   tags = {
     Name = "Terraform_Web_App"
   }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
 }
 
-# Output
+# ---------------- Output ----------------
 output "public_ip" {
   value = aws_instance.web_server.public_ip
 }
